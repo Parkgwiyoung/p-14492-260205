9f1f5f166fcf1a6b6ef274acf0e51b11dbf5b99b
Parkgwiyoung <pgwi11@naver.com>
2026-02-06 11:16:08 +0900

    6 - b

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,51 +2,47 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-//        experiment1();
-        experiment2();
+        lab2();
     }
 
-    public static void experiment2() {
-        System.out.println("안녕하세요");
+    public static void lab2() {
 
-        PrintStream originalOut = System.out;
+        PrintStream ORIGINAL_OUT = System.out;
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
 
         System.setOut(printStream);
 
-        System.out.println("하하하");
+        System.out.println("hihi");
+        System.out.println("byebye");
+        System.out.print("ok");
 
-        String outStr = outputStream.toString();
-        System.setOut(originalOut);
-        printStream.close();
+        System.setOut(ORIGINAL_OUT); // 모니터로 출력 복원
 
-        if(outStr.equals("하하하")) {
-            System.out.println("출력 결과가 일치합니다.");
-        } else {
-            System.out.println("출력 결과가 일치하지 않습니다.");
-        }
+        String result = byteArrayOutputStream.toString();
 
-        System.out.println(outStr);
-
-
+        System.out.println(result);
     }
 
-    public static void experiment1() {
-
-        String input = """
+    public static void lab1() {
+        Scanner scan = new Scanner("""
                 등록
-                너 자신을 알라
-                """;
-        Scanner sc = new Scanner(input);
-        String cmd = sc.nextLine();
-        String saying = sc.nextLine();
-        System.out.println("입력한 명령어: " + cmd);
-        System.out.println("입력한 명언: " + saying);
+                과거에 집착하지 마라.
+                작자미상
+                """);
+
+        String str1 = scan.nextLine();
+        String str2 = scan.nextLine();
+        String str3 = scan.nextLine();
+
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
 
     }
-
 }
--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,19 +1,11 @@
 import java.io.ByteArrayOutputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
 
-//    public static String run(String input) {
-//
-//        // 입력값
-//        Scanner scan = new Scanner(input);
-//
-//        // 명언 프로그램 실행
-//
-//        // 출력값
-//    }
+    private static PrintStream ORIGINAL_OUT = System.out;
+    private static PrintStream CURRENT_OUT = System.out;
 
     public static Scanner genScanner(String input) {
         return new Scanner(input);
@@ -25,7 +17,14 @@ public class TestUtil {
         PrintStream printStream = new PrintStream(byteArrayOutputStream);
 
         System.setOut(printStream);
+        CURRENT_OUT = printStream;
 
         return byteArrayOutputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws Exception {
+        System.setOut(ORIGINAL_OUT);
+        outputStream.close();
+        CURRENT_OUT.close();
     }
 }
