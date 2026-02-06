ac8d191d547fb5acda08060c8e9a30e0066dd7b2
Parkgwiyoung <pgwi11@naver.com>
2026-02-06 10:30:41 +0900

    5ê - rg

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TestUtil {
+
+    public static String run(String input) {
+
+        Scanner scan = new Scanner(input);
+        String cmd = scan.nextLine();
+
+
+    }
+
+    public static Scanner genScanner(String input) {
+        return new Scanner(input);
+    }
+}
